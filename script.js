fbeb54111a1bdae39c4dec3d1612a3f28fc98c7b
Pahan <122900717+Pahan3568@users.noreply.github.com>
2025-01-27 10:49:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ document.addEventListener('DOMContentLoaded', () => {
             return;
         }
 
-        const days = Math.floor(timeLeft / (1000 × 60 × 60 * 24));
-        const hours = Math.floor((timeLeft % (1000 × 60 × 60 * 24)) / (1000 × 60 × 60));
-        const minutes = Math.floor((timeLeft % (1000 × 60 × 60)) / (1000 * 60));
+        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
         updateValue(daysElement, days);
